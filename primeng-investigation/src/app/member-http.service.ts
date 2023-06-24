3f63f26db27ca23f898c4345f9fff9de4988a9e1
SimplicityIsArt <93212168+SimplicityIsAnArt@users.noreply.github.com>
2023-06-24 01:54:28 +0100

    Add tap for performing side-effects in the Observalbe Stream corrrectly. ( i.e. writing to the console in the outside world is a side effect, but we don't modify the origional data. It's as if it passes straight through

--- primeng-investigation/src/app/member-http.service.ts
+++ primeng-investigation/src/app/member-http.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { Member } from './member.model';
 
 @Injectable({
@@ -20,24 +20,28 @@ export class MemberHttpService {
 
   getAllMembers(): Observable<Member[]> {
     return this.http.get<Member[]>(this.url).pipe(
+      tap(() => console.log('Fetched all members')),
       catchError(this.handleError)
     );
   }
 
   delete(id: number): Observable<{}> {
     return this.http.delete(`${this.url}/${id}`).pipe(
+      tap(() => console.log(`Deleted member id=${id}`)),
       catchError(this.handleError)
     );
   }
 
   add(member: Member): Observable<Member> {
     return this.http.post<Member>(this.url, member).pipe(
+      tap((newMember) => console.log(`Added member with id=${newMember.id}`)),
       catchError(this.handleError)
     );
   }
 
   update(member: Member): Observable<Member> {
     return this.http.put<Member>(`${this.url}/${member.id}`, member).pipe(
+      tap(() => console.log(`Updated member id=${member.id}`)),
       catchError(this.handleError)
     );
   }
