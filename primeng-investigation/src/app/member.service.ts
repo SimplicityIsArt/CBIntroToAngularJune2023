a73fa9d4f331c35b60887056336ce5ec54a5e8f9
SimplicityIsArt <93212168+SimplicityIsAnArt@users.noreply.github.com>
2023-06-24 14:09:52 +0100

    Refactor MemberService for increased modularity, readability, and testability
    - Separate responsibilities between MemberService and MemberHttpService for clearer, more modular code
    
    - Switch from directly managing data to returning Observables for increased flexibility
    
    - Centralize error handling in MemberHttpService, throwing errors to be caught by subscribers
    
    - Ensure all MemberService methods return Observables for consistency
    
    - Improve readability through simplified, single-responsibility methods
    
    - Enable independent testing of MemberService and MemberHttpService

--- primeng-investigation/src/app/member.service.ts
+++ primeng-investigation/src/app/member.service.ts
@@ -1,46 +1,28 @@
 import { Injectable } from '@angular/core';
 import { MemberHttpService } from './member-http.service';
 import { Member } from './member.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
 
-  members: Member[] = [];
+  constructor(private memberHttpService: MemberHttpService) { }
 
-  constructor(private memberHttpService: MemberHttpService) {
-    this.memberHttpService.getAllMembers()
-      .subscribe((members: Member[]) => {
-        this.members = members;
-      });
+  getAll(): Observable<Member[]> {
+    return this.memberHttpService.getAllMembers();
   }
 
-  getAll(): Member[] {
-    return this.members;
+  update(memberToUpdate: Member): Observable<Member> {
+    return this.memberHttpService.update(memberToUpdate);
   }
 
-  update(memberToUpdate: Member) {
-    this.memberHttpService.update(memberToUpdate)
-      .subscribe((updatedMember)=> {
-        let index = this.members.findIndex(member=>member.id == memberToUpdate.id);
-        this.members.splice(index, 1, updatedMember);
-      });
+  delete(id: number): Observable<{}> {
+    return this.memberHttpService.delete(id);
   }
 
-  delete(id: number) {
-    this.memberHttpService.delete(id)
-      .subscribe(()=>{
-        let index = this.members.findIndex(member=>member.id == id);
-        this.members.splice(index, 1);
-      });
-  }
-
-  add(memberToAdd: Member): Member {
-    this.memberHttpService.add(memberToAdd)
-      .subscribe(addedMember => {
-        this.members.push(addedMember);
-      });
-      return memberToAdd;
+  add(memberToAdd: Member): Observable<Member> {
+    return this.memberHttpService.add(memberToAdd);
   }
 }
